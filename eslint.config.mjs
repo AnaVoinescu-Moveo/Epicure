ae26e58f6216c5dafd85718a363f0fb1fb1e5693
AnaVoinescu-Moveo <ana.voinescu@moveo.co.il>
2026-06-08 14:26:55 +0300

    fix: resolve strapi lint errors in ci

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist', '**/out-tsc', '**/test-output'],
+    ignores: ['**/dist', '**/out-tsc', '**/test-output', 'packages/strapi/types/generated/**'],
   },
   {
     files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
--- packages/strapi/src/index.ts
+++ packages/strapi/src/index.ts
@@ -7,6 +7,7 @@ export default {
    *
    * This gives you an opportunity to extend code.
    */
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   register(/* { strapi }: { strapi: Core.Strapi } */) {},
 
   /**
@@ -16,5 +17,6 @@ export default {
    * This gives you an opportunity to set up your data model,
    * run jobs, or perform some special logic.
    */
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   bootstrap(/* { strapi }: { strapi: Core.Strapi } */) {},
 };
